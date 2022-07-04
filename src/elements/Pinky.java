97332852e922186376aa231297d04867d6ff3e00
Guilherme <narcisolee2002@gmail.com>
2022-07-04 16:46:53 -0300

    estruturando Jmenu-1

---
+++ src/elements/Pinky.java
@@ -0,0 +1,43 @@
+package elements;
+
+import utils.Drawing;
+import utils.Position;
+
+import java.awt.Graphics;
+import java.io.Serializable;
+import java.util.Random;
+public class Pinky extends Ghost  {
+     
+	public Pinky(String imageName) {
+	      super(imageName);
+	}
+    @Override
+    public void autoDraw(Graphics g){
+    	Pacman pacman=Drawing.getGameScreen().getPacman();
+        Position posPacman=pacman.getPos();
+        int movDirectionPacman=pacman.getMoveDirection();
+		
+		//NOTE: MUDANÇA DE MOVIMENTO PARALELO PRA PINKY
+        if (movDirectionPacman==MOVE_LEFT ||movDirectionPacman==MOVE_RIGHT){
+        	if(!this.isMortal){
+        		followPacmanHorizontal(movDirectionPacman, posPacman);
+        	}
+        	else{
+        		escapePacmanHorizontal(movDirectionPacman, posPacman);
+        	}
+        }
+        else if(movDirectionPacman==MOVE_DOWN ||movDirectionPacman==MOVE_UP){
+        	if (!this.isMortal) {
+				followPacmanVertical(movDirectionPacman, posPacman);		
+			} else {
+				escapePacmanVertical(movDirectionPacman, posPacman);
+			}
+
+        }
+        Drawing.draw(g, this.imageIcon, pos.getY(), pos.getX());
+
+    }
+
+
+
+}
